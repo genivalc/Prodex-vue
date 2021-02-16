a2ecf76e987a2ae7bf0a9ad16fa4e519660c0756
Genival Neto <genivalcandeiadon.neto@gmail.com>
2021-02-16 15:23:51 -0300

    Atulizando

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div id="app">
+    <div class="column is-half is-offset-one-quarter">
+    <div v-for="(poke,index) in pokemons" :key="index">
+      <pokemon :name="poke.name" :url="poke.url" :num="index+1" />
+    </div>
+    </div>
+
 
   </div>
 </template>
 
 <script>
-//import axios from "axios";
+import axios from "axios";
+import Pokemon from './components/Pokemon.vue';
 export default {
   name: 'App',
+  data(){
+    return {
+      pokemons: []
+    }
+  },
+  created: function(){
+  axios.get("https://pokeapi.co/api/v2/pokemon?limit=153&offset=251").then(res => {
+      this.pokemons = res.data.results;
+    })
+  },
+  components: {
+    Pokemon
+  }
 
 }
 </script>
